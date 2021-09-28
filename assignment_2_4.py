31cc0c2ccf2a8784cb9e39e5fd9b191cc321c76f
KibetAmon <kibetngetich805@gmail.com>
2021-09-28 22:06:03 +0300

    lecture 2

--- assignment_2_4.py
+++ assignment_2_4.py
@@ -1,5 +1,11 @@
 #a program that computes a given integer power of given number
 
 def main():
-    number = int(input("Enter a number": ))
-    expo = int(input("Enter the power"))
+    number = int(input("Enter a number:" ))
+    expo = int(input("Enter the power: "))
+
+    for expo in range(expo+1):
+        answer = number ** expo
+        print("answer is: ", answer)
+
+main()
